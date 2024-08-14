5fca48405add87cde8bea51baa6471f41cb15be6
Diego Pertierra <dpertierra@hotmail.com>
2024-08-14 18:04:51 +0200

    Upload Essentials 16/BES Version

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 Este plugin les permitirá hacer un desafío mono tipo en sus fangames. El jugador podrá elegir el tipo y solo se le permitirá capturar a los Pokémon de ese tipo o que tengan una evolucion de ese tipo. Al activarlo le eliminará del equipo todos sus Pokémon que no cumplan con el tipo, si no tiene ningun Pokémon que cumpla con el tipo, se le dará a elegir entre 3 nuevos iniciales del tipo elegido, de un listado definido en el script.
 
+![captura 1](assets/capture1.png)
+![captura 2](assets/capture2.png)
+![captura 3](assets/capture3.png)
+
+### Con el flag `BLOQUEAR_EVOLUCIONES_A_OTROS_TIPOS` activado
+
+#### Con piedra agua
+
+![imagen water_stone](assets/water_stone.png)
+
+#### Con piedra fuego
+
+![imagen fire_stone](assets/water_stone.png)
+
 ## Configuración
 
 En el script hay una constante llamada `TYPES` en esta constante se definen los tipos que consideran que en sus juegos es viable hacer un reto monotipo.
@@ -14,17 +28,17 @@ En el script hay una constante llamada `TYPES` en esta constante se definen los 
 
 ## Implementación
 
-1. Descargar el script del ultimo [release](https://github.com/Pokemon-Fan-Games/Modo-Monotype/releases/latest).
+1. Descargar el script del ultimo [release](https://github.com/Pokemon-Fan-Games/Modo-Monotype/releases/tag/16_BES_v1.0.0).
 2. Crear el siguiente evento para activar el reto.
-   1. ![imagen evento](/assets/evento1.png)
+   1. ![imagen evento](/assets/event1.png)
    2. Primero hay que hacer un show choices preguntando si se desea activar el modo.
-   3. Pidan una confirmacion ya que al activarlo se le borrarán todos los Pokemón del equipo que no cumplan con el tipo.
+   3. Pidan una confirmacion ya que al activarlo se borrará el primer pokemon del equipo.
    4. Una vez confirmado hay que agregar al evento una sentencia de script con el siguiente código:
       ```ruby
-       options = MonotypeChallenge.get_type_options
-       ret = pbMessage("Elige el tipo para tu reto", options)
+       options = MonotypeChallenge.type_options
+       ret = Kernel.pbMessage('Elige el tipo para tu reto', options)
        MonotypeChallenge.type = ret
       ```
    5. Luego de esto ponen una conditional branch de tipo script con la siguiente linea `MonotypeChallenge.enabled?`
-   6. En el true de ese conditional branch ponen otra conditional branch que es la que se encargará de eliminar a los pokemon no validos del equipo. La conditional branch debe tener la siguiente linea: `MonotypeChallenge.no_valid_pokemon_in_party?`
+   6. En el true de ese conditional branch ponen otra conditional branch que es la que se encargará de eliminar a los pokemon no validos del equipo. La conditional branch debe tener la siguiente linea: `!MonotypeChallenge.valid_monotype?($Trainer.party[0])`
    7. En el true de esa conditional branch llaman al siguiente script para que el jugador pueda elegir a su nuevo inicial `MonotypeChallenge.choose_starter`
---
+++ monotype.rb
@@ -0,0 +1,143 @@
+#####################################################################################
+#                          CRÉDITOS/CREDITS: DPertierra                                     #
+#####################################################################################
+
+class PokemonGlobalMetadata
+  attr_accessor :monotype_type
+end
+module MonotypeChallenge
+  BLOQUEAR_EVOLUCIONES_A_OTROS_TIPOS = false
+
+  # Listado de tipos posibles para el reto monotype
+  TYPES = [ PBTypes::BUG, PBTypes::NORMAL, PBTypes::POISON,
+            PBTypes::FLYING, PBTypes::WATER, PBTypes::GRASS,
+            PBTypes::FIRE
+          ]
+
+  # Listado de iniciales para cada reto monotype
+  # Pueden ser mas de 3 y se tomarán 3 de este listado aleatoriamente.
+  STARTER_OPTIONS = {
+    PBTypes::BUG    => [PBSpecies::SEWADDLE, PBSpecies::GRUBBIN, PBSpecies::BLIPBUG],
+    PBTypes::NORMAL => [PBSpecies::WHISMUR, PBSpecies::LILLIPUP, PBSpecies::ZIGZAGOON],
+    PBTypes::POISON => [PBSpecies::ODDISH, PBSpecies::ZUBAT, PBSpecies::VENIPEDE],
+    PBTypes::FLYING => [PBSpecies::STARLY, PBSpecies::PIKIPEK, PBSpecies::ROOKIDEE],
+    PBTypes::WATER  => [PBSpecies::POLIWAG, PBSpecies::HORSEA, PBSpecies::TYMPOLE],
+    PBTypes::GRASS  => [PBSpecies::SEEDOT, PBSpecies::BUDEW, PBSpecies::SMOLIV],
+    PBTypes::FIRE   => [PBSpecies::MAGBY, PBSpecies::LITWICK, PBSpecies::ROLYCOLY]
+  }
+
+  def self.enabled?
+    type ? true : false
+  end
+
+  # Devuelve el tipo para el modo monotype
+  def self.type
+    $PokemonGlobal.monotype_type || nil
+  end
+
+  # Guarda el tipo del monotype
+  def self.type=(type_index)
+    return if type_index >= self::TYPES.length
+
+    selected_type = self::TYPES[type_index]
+    $PokemonGlobal.monotype_type = selected_type
+  end
+
+  # Devuelve los tipos posibles para el monotype
+  def self.type_options
+    options = self::TYPES.map { |type| PBTypes.getName(type) }
+    options.push(_INTL('NO'))
+    options
+  end
+
+  # Devuelve 3 starters para el monotype seleccionado
+  def self.choose_starter
+    return unless $PokemonGlobal.monotype_type
+
+    starters = self::STARTER_OPTIONS[$PokemonGlobal.monotype_type]
+    starters.shuffle!
+    commands = []
+
+    (0...3).each do |i|
+      starter = starters[i]
+      commands.push(PBSpecies.getName(starter))
+    end
+
+    chosen = -1
+    chosen = Kernel.pbMessage(_INTL('Elige a tu nuevo inicial'), commands, -1) while chosen == -1
+
+    pbAddPokemon(starters[chosen], 5)
+    pbRemovePokemonAt(0)
+    type_name = PBTypes.getName(type)
+    Kernel.pbMessage(_INTL("¡A partir de ahora estás en un <b>Reto Monotype</b> de tipo #{type_name}!"))
+  end
+
+  # Valida que el pokemon sea valido para el reto monotype elegido
+  # Devuelve mensaje de error si no lo es
+  def self.valid_monotype_with_text?(poke)
+    selected_type = type
+    return true if selected_type.nil? # No está activo el monotype
+
+    if poke.is_a?(Integer)
+      species = poke
+      dexdata = pbOpenDexData
+      pbDexDataOffset(dexdata, species, 8) # Type
+      type1 = dexdata.fgetb
+      type2 = dexdata.fgetb
+
+      evos_types = evolved_types(species)
+
+      unless [type1, type2].include?(selected_type) || evos_types.include?(selected_type)
+        return false, PBTypes.getName(selected_type)
+      end
+
+    else
+      evos_types = evolved_types(poke)
+
+      unless poke.pbHasType?(selected_type) || evos_types.include?(selected_type)
+        return false, PBTypes.getName(selected_type)
+      end
+    end
+    true
+  end
+
+  # Valida que el pokemon sea valido para el monotype elegido
+  # Devuelve true si lo es, y false si no
+  def self.valid_monotype?(poke)
+    is_valid, _text = valid_monotype_with_text?(poke)
+    is_valid
+  end
+
+  # Valida si alguna de las evoluciones del pokemon tiene el tipo
+  # del monotype elegido
+  def self.evolved_types(poke)
+    species = poke.is_a?(Integer) ? poke : poke.species
+
+    evos = pbGetEvolvedFormData(species)
+    dexdata = pbOpenDexData
+    evos_types = []
+
+    until evos.empty?
+      pbDexDataOffset(dexdata, evos.first[2], 8) # Type
+      type1 = dexdata.fgetb
+      type2 = dexdata.fgetb
+      evos_types.push(type1, type2)
+      evos = pbGetEvolvedFormData(evos.first[2])
+    end
+
+    evos_types
+  end
+end
+
+# Bloquea la evolucion si no tiene el tipo elegido para el reto monotype
+if MonotypeChallenge::BLOQUEAR_EVOLUCIONES_A_OTROS_TIPOS
+  alias pbCheckEvolution_mono pbCheckEvolution
+  def pbCheckEvolution(pokemon, item = 0)
+    new_species = pbCheckEvolution_mono(pokemon, item)
+    return -1 if new_species < 0
+
+    return -1 unless MonotypeChallenge.valid_monotype?(new_species)
+
+    new_species
+  end
+end
